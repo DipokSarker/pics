45df3e055ac44a14b7cc339de7f84b0a28376fb5
DipokSarker <dipok.ronjan.sarker@gmail.com>
2023-04-13 00:10:18 +0300

    state and props

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const searchImages = async(term) => {
-    const response = await axios.get('api.unsplash.com/search/photos', {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID kfW8XgaxM0t9GEmy5gsBnTKZghLuS3g5oey05XWleek'
         },
-        param: {
+        params: {
             query: term,
         }
 });
